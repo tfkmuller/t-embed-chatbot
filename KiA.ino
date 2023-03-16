52bf5bd66870a210e06f0b0d33763be7601c7718
Tom <33751239+tfkmuller@users.noreply.github.com>
2023-03-16 17:17:44 +0000

    Update KiA.ino

--- KiA.ino
+++ KiA.ino
@@ -8,27 +8,49 @@
 #include <WiFiClientSecure.h>
 #include <ArduinoJson.h>
 #include "ChatGPT.hpp"
-#include "pin_config.h"
-#include "SD_MMC.h"
-#include <OneButton.h>
+#include <TFT_eSPI.h>
 #include <RotaryEncoder.h>
-#include "TFT_eSPI.h"
-#include "APA102.h"
-#include "Audio.h" 
-#include <LittleFS.h>
+#include <FastLED.h>
+#include "FS.h"
+#include "SD_MMC.h"
+#include <JPEGDecoder.h>
+#include "Audio.h"      // https://github.com/schreibfaul1/ESP32-audioI2S
+#include <OneButton.h>     // https://github.com/mathertel/OneButton
 #include "Arduino.h"
 #include "driver/i2s.h"
-#include "es7210.h"
+#include "pin_config.h"
+#include "Wire.h"
+//#include "audio_hal.h"
+//#include "es7210.h"
+//#include "AudioTools.h"
+CRGB leds[NUM_LEDS];
 
+File file;
+//const char filename[] = "/recording.wav";
+const int headerSize = 44;
 
-File myFile;
+unsigned short colorsF[3] = {TFT_WHITE, TFT_BLACK, TFT_WHITE};
+unsigned short colorsB[3] = {TFT_BLACK, color5, TFT_BLACK};
+unsigned short colorsL[3] = {TFT_RED, TFT_BLACK, TFT_RED};
+unsigned short colorsD[3] = {TFT_WHITE, TFT_BLACK, TFT_RED};
+volatile int counter = 0;
+float VALUE[3];
+float lastValue = 0;
 
-TFT_eSPI tft = TFT_eSPI(170, 320);
+const int default_webserverporthttp = 80;
+const int pwmFreq = 5000;
+const int pwmResolution = 8;
+const int pwmLedChannelTFT = 0;
 
-RotaryEncoder encoder(PIN_ENCODE_A, PIN_ENCODE_B, RotaryEncoder::LatchMode::TWO03);
-OneButton button(PIN_ENCODE_BTN, true);
-APA102<PIN_APA102_DI, PIN_APA102_CLK> ledStrip;
-Audio *audio;
+const int ledPin2 = 27;
+const int freq2 = 5000;
+const int ledChannel2 = 2;
+const int resolution2 = 8;
+int dutyCycle2 = 128;
+int deb = 0;
+int angle[3] = {270};
+bool onOff = 0;
+bool debounce = 0;
 
 
 const char *test_root_ca =
@@ -64,60 +86,136 @@ const char *test_root_ca =
   "emyPxgcYxn/eR44/KJ4EBs+lVDR3veyJm+kXQ99b21/+jh5Xos1AnX5iItreGCc=\n"
   "-----END CERTIFICATE-----\n";
 
-bool buttonState = false;
-
 WiFiClientSecure client;
-ChatGPT<WiFiClientSecure> chat_gpt(&client, "v1", "<OpenAI_API_KEY>");
+ChatGPT<WiFiClientSecure> chat_gpt(&client, "v1", "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");
+
+TFT_eSPI tft = TFT_eSPI();
+TFT_eSprite img = TFT_eSprite(&tft);
+
+
+RotaryEncoder encoder(PIN_IN1, PIN_IN2, RotaryEncoder::LatchMode::TWO03);
+int chosen = 0;
+OneButton button(PIN_ENCODE_BTN, true);
+
+//Audio *audio;
+Audio audio;
 
 void setup() {
-    pinMode(PIN_POWER_ON, OUTPUT);
-  digitalWrite(PIN_POWER_ON, HIGH);
+
+  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0);
+  pinMode(0, INPUT_PULLUP);
+  pinMode(46, OUTPUT);
+  digitalWrite(46, HIGH);
+
+  pinMode(15, OUTPUT);
+  digitalWrite(15, HIGH);
+
+  FastLED.addLeds<APA102, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
+  FastLED.clear();
+  FastLED.show();
+
   Serial.begin(115200);
   Serial.print("Connecting to WiFi network: ");
   Serial.print(WIFI_SSID);
   Serial.println("'...");
-   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+  button.attachClick(click1);
+  button.attachDoubleClick(doubleclick1);
+  button.attachLongPressStart(longPressStart1);
+  button.attachLongPressStop(longPressStop1);
+  button.attachDuringLongPress(longPress1);
+
+
+  tft.begin();
+  tft.writecommand(0x11);
+  tft.setRotation(3);
+  tft.setSwapBytes(true);
+  tft.fillScreen(TFT_BLACK);
+  tft.setTextDatum(4);
+
+  img.setSwapBytes(true);
+  img.createSprite(320, 170);
+
+
+  SD_init();
+  drawSdJpeg("/tomuelle.jpg", 0, 0);
+  //i2s_init();
+
+  readtext();
+
+
+audio.setPinout(PIN_IIS_BCLK , PIN_IIS_WCLK , PIN_IIS_DOUT);
+    //audio.setVolume(21); // 0...21
+    audio.connecttoFS(SD_MMC,"/book.mp3");
+
+  audio.setVolume(21); 
+
+    
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 
   while (WiFi.status() != WL_CONNECTED) {
     Serial.println("Connecting...");
     delay(500);
   }
   Serial.println("Connected!");
-tft.begin();
+  Serial.println(WiFi.localIP());
 
-  tft.setRotation(3);
-  tft.fillScreen(TFT_BLACK);
+
   //For SSL Connection (HTTPS)
   client.setCACert(test_root_ca);
 
-  String result;
- pinMode(PIN_LCD_BL, OUTPUT);
-  digitalWrite(PIN_LCD_BL, HIGH);
-  //tft.println("Wifi connected");
-  SD_init();
-//tft.fillCircle(64, 64, 61, TFT_BLUE);
+  //  String result;
 
-  Serial.println("[ChatGPT] Only print a content message");
-  if (chat_gpt.simple_message("gpt-3.5-turbo-0301", "user", "Planning a 3-day trip to San Diego", result)) {
-    Serial.println("===OK===");
-    Serial.println(result);
-  } else {
-    Serial.println("===ERROR===");
-    Serial.println(result);
-  }
+  //  Serial.println("This is the answer:");
+  //  if (chat_gpt.simple_message("gpt-3.5-turbo-0301", "user", "What's a Japanese greeting?", result)) {
+  //    //Serial.println("===OK===");
+  //    Serial.println(result);
+  //    tft.setTextColor(TFT_RED);
+  //    tft.setTextSize(2);
+  //    //tft.setCursor(20, 30);
+  //    tft.setCursor(0, 0);
+  //    tft.println(result);
+  //  } else {
+  //    Serial.println("===ERROR===");
+  //    Serial.println(result);
+  //
+  //  }
 
-  Serial.println("\n\n[ChatGPT] Print full message(JSON Type)");
-  if (chat_gpt.full_message("gpt-3.5-turbo", "user", "What is the OpenAI mission?", result)) {
-    Serial.println("===OK===");
-    Serial.println(result);
-  } else {
-    Serial.println("===ERROR===");
-    Serial.println(result);
-  }
 }
 
-void loop() {}
-void SD_init(void) {
+void loop() {
+  button.tick();
+  static int pos = 0;
+  encoder.tick();
+
+  int newPos = encoder.getPosition();
+
+//audio.setVolumeSteps(100); // max 255
+  
+  
+  if (pos != newPos) {
+    //prints actual value of encoder step
+    //Serial.println(newPos);
+    if (pos > newPos)
+    {
+      Serial.println("forward");
+      drawSdJpeg("/peter.jpg", 0, 0);
+      Serial.println(newPos);
+    }
+    if (pos < newPos)
+    {
+      Serial.println("back");
+      drawSdJpeg("/kids.jpg", 0, 0);
+    }
+    pos = newPos;
+
+  }
+
+//audio.loop();
+
+}
+
+void SD_init() {
   pinMode(PIN_SD_CS, OUTPUT);
   digitalWrite(PIN_SD_CS, 1);
   SD_MMC.setPins(PIN_SD_SCK, PIN_SD_MOSI, PIN_SD_MISO);
@@ -125,17 +223,365 @@ void SD_init(void) {
     Serial.println("Card Mount Failed");
     return;
   }
-
-  File root = SD_MMC.open("/");
-  File file = root.openNextFile();
-
   uint8_t cardType = SD_MMC.cardType();
 
   if (cardType == CARD_NONE) {
     Serial.println("No SD card attached");
     return;
   }
-  uint64_t cardSize = SD_MMC.cardSize() / (1024 * 1024);
-  Serial.printf("SD Card Size: %lluMB\n", cardSize);
+
   delay(500);
+
+}
+
+
+void readtext()
+{
+  File textfile = SD_MMC.open("/response.txt");
+  if (!textfile) {
+    Serial.println("Failed to open file for reading");
+    return;
+  }
+
+  Serial.println("File Content:");
+  while (textfile.available()) {
+    Serial.write(textfile.read());
+  }
+  textfile.close();
+}
+//####################################################################################################
+// Draw a JPEG on the TFT pulled from SD Card
+//####################################################################################################
+// xpos, ypos is top left corner of plotted image
+void drawSdJpeg(const char *filename, int xpos, int ypos) {
+
+  // Open the named file (the Jpeg decoder library will close it)
+  File jpegFile = SD_MMC.open( filename, FILE_READ);  // or, file handle reference for SD library
+
+  if ( !jpegFile ) {
+    Serial.print("ERROR: File \""); Serial.print(filename); Serial.println ("\" not found!");
+    return;
+  }
+
+  Serial.println("===========================");
+  Serial.print("Drawing file: "); Serial.println(filename);
+  Serial.println("===========================");
+
+  // Use one of the following methods to initialise the decoder:
+  bool decoded = JpegDec.decodeSdFile(jpegFile);  // Pass the SD file handle to the decoder,
+  //bool decoded = JpegDec.decodeSdFile(filename);  // or pass the filename (String or character array)
+
+  if (decoded) {
+    // print information about the image to the serial port
+    // jpegInfo();
+    // render the image onto the screen at given coordinates
+    jpegRender(xpos, ypos);
+  }
+  else {
+    Serial.println("Jpeg file format not supported!");
+  }
+}
+
+//####################################################################################################
+// Draw a JPEG on the TFT, images will be cropped on the right/bottom sides if they do not fit
+//####################################################################################################
+// This function assumes xpos,ypos is a valid screen coordinate. For convenience images that do not
+// fit totally on the screen are cropped to the nearest MCU size and may leave right/bottom borders.
+void jpegRender(int xpos, int ypos) {
+
+  //jpegInfo(); // Print information from the JPEG file (could comment this line out)
+
+  uint16_t *pImg;
+  uint16_t mcu_w = JpegDec.MCUWidth;
+  uint16_t mcu_h = JpegDec.MCUHeight;
+  uint32_t max_x = JpegDec.width;
+  uint32_t max_y = JpegDec.height;
+
+  bool swapBytes = tft.getSwapBytes();
+  tft.setSwapBytes(true);
+
+  // Jpeg images are draw as a set of image block (tiles) called Minimum Coding Units (MCUs)
+  // Typically these MCUs are 16x16 pixel blocks
+  // Determine the width and height of the right and bottom edge image blocks
+  uint32_t min_w = jpg_min(mcu_w, max_x % mcu_w);
+  uint32_t min_h = jpg_min(mcu_h, max_y % mcu_h);
+
+  // save the current image block size
+  uint32_t win_w = mcu_w;
+  uint32_t win_h = mcu_h;
+
+  // record the current time so we can measure how long it takes to draw an image
+  uint32_t drawTime = millis();
+
+  // save the coordinate of the right and bottom edges to assist image cropping
+  // to the screen size
+  max_x += xpos;
+  max_y += ypos;
+
+  // Fetch data from the file, decode and display
+  while (JpegDec.read()) {    // While there is more data in the file
+    pImg = JpegDec.pImage ;   // Decode a MCU (Minimum Coding Unit, typically a 8x8 or 16x16 pixel block)
+
+    // Calculate coordinates of top left corner of current MCU
+    int mcu_x = JpegDec.MCUx * mcu_w + xpos;
+    int mcu_y = JpegDec.MCUy * mcu_h + ypos;
+
+    // check if the image block size needs to be changed for the right edge
+    if (mcu_x + mcu_w <= max_x) win_w = mcu_w;
+    else win_w = min_w;
+
+    // check if the image block size needs to be changed for the bottom edge
+    if (mcu_y + mcu_h <= max_y) win_h = mcu_h;
+    else win_h = min_h;
+
+    // copy pixels into a contiguous block
+    if (win_w != mcu_w)
+    {
+      uint16_t *cImg;
+      int p = 0;
+      cImg = pImg + win_w;
+      for (int h = 1; h < win_h; h++)
+      {
+        p += mcu_w;
+        for (int w = 0; w < win_w; w++)
+        {
+          *cImg = *(pImg + w + p);
+          cImg++;
+        }
+      }
+    }
+
+    // calculate how many pixels must be drawn
+    uint32_t mcu_pixels = win_w * win_h;
+
+    // draw image MCU block only if it will fit on the screen
+    if (( mcu_x + win_w ) <= tft.width() && ( mcu_y + win_h ) <= tft.height())
+      tft.pushImage(mcu_x, mcu_y, win_w, win_h, pImg);
+    else if ( (mcu_y + win_h) >= tft.height())
+      JpegDec.abort(); // Image has run off bottom of screen so abort decoding
+  }
+
+  tft.setSwapBytes(swapBytes);
+
+}
+
+
+void click1()
+{
+  Serial.println("Button pressed");
+  leds[0] = CRGB::Green;
+  leds[1] = CRGB::Green;
+  leds[2] = CRGB::Green;
+  leds[3] = CRGB::Green;
+  leds[4] = CRGB::Green;
+  leds[5] = CRGB::Green;
+  leds[6] = CRGB::Green;
+  FastLED.show();
+  delay(150);
+  FastLED.clear();
+  FastLED.show();
+ 
+
+  String result;
+  Serial.println("Listing files:");
+  listFiles("/");
+  Serial.println("This is the answer:");
+  if (chat_gpt.simple_message("gpt-3.5-turbo-0301", "user", "What's a Japanese greeting?", result)) {
+    //Serial.println("===OK===");
+    Serial.println(result);
+    tft.setTextColor(TFT_RED);
+    tft.setTextSize(2);
+    //tft.setCursor(20, 30);
+    tft.setCursor(0, 0);
+    tft.println(result);
+  } else {
+    Serial.println("===ERROR===");
+    Serial.println("mit ChatGPT");
+    Serial.println(result);
+
+  }
+  
+}
+
+void doubleclick1()
+{
+  //audio->loop();
+  }
+void longPressStart1()
+{}
+void longPressStop1()
+{}
+void longPress1()
+{}
+
+
+
+void i2s_init(void) {
+  i2s_config_t i2s_config = {
+    .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_TX),
+    .sample_rate = SAMPLE_FREQ,
+    .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,
+    .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,
+    .communication_format = I2S_COMM_FORMAT_STAND_I2S,
+    .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
+    .dma_buf_count = 6,
+    //      .dma_buf_count = 64,
+    .dma_buf_len = SAMPLE_BLOCK,
+    //      .use_apll = 1,
+    //      .tx_desc_auto_clear = true,
+    .use_apll = false,
+    .tx_desc_auto_clear = true,
+    .fixed_mclk = 0,
+    .mclk_multiple = I2S_MCLK_MULTIPLE_256,
+    .bits_per_chan = I2S_BITS_PER_CHAN_16BIT,
+
+  };
+  i2s_pin_config_t pin_config = {0};
+  pin_config.bck_io_num = PIN_IIS_BCLK;
+  pin_config.ws_io_num = PIN_IIS_WCLK;
+  pin_config.data_out_num = PIN_IIS_DOUT;
+  pin_config.data_in_num = PIN_ES7210_DIN;
+  pin_config.mck_io_num = PIN_ES7210_MCLK;
+
+  i2s_driver_install(I2S_NUM_1, &i2s_config, 0, NULL);
+  i2s_set_pin(I2S_NUM_1, &pin_config);
+  i2s_zero_dma_buffer(I2S_NUM_1);
+  delay(100);
+}
+
+
+void i2s_adc_data_scale(uint8_t * d_buff, uint8_t* s_buff, uint32_t len)
+{
+  uint32_t j = 0;
+  uint32_t dac_value = 0;
+  for (int i = 0; i < len; i += 2) {
+    dac_value = ((((uint16_t) (s_buff[i + 1] & 0xf) << 8) | ((s_buff[i + 0]))));
+    d_buff[j++] = 0;
+    d_buff[j++] = dac_value * 256 / 2048;
+  }
+}
+
+void i2s_adc(void *arg)
+{
+
+  int i2s_read_len = I2S_READ_LEN;
+  int flash_wr_size = 0;
+  size_t bytes_read;
+
+  char* i2s_read_buff = (char*) calloc(i2s_read_len, sizeof(char));
+  uint8_t* flash_write_buff = (uint8_t*) calloc(i2s_read_len, sizeof(char));
+
+  i2s_read(I2S_PORT, (void*) i2s_read_buff, i2s_read_len, &bytes_read, portMAX_DELAY);
+  i2s_read(I2S_PORT, (void*) i2s_read_buff, i2s_read_len, &bytes_read, portMAX_DELAY);
+
+  Serial.println(" *** Recording Start *** ");
+  while (flash_wr_size < RECORD_SIZE) {
+    //read data from I2S bus, in this case, from ADC.
+    i2s_read(I2S_PORT, (void*) i2s_read_buff, i2s_read_len, &bytes_read, portMAX_DELAY);
+    //example_disp_buf((uint8_t*) i2s_read_buff, 64);
+    //save original data from I2S(ADC) into flash.
+    i2s_adc_data_scale(flash_write_buff, (uint8_t*)i2s_read_buff, i2s_read_len);
+    file.write((const byte*) flash_write_buff, i2s_read_len);
+    flash_wr_size += i2s_read_len;
+    ets_printf("Sound recording %u%%\n", flash_wr_size * 100 / RECORD_SIZE);
+    ets_printf("Never Used Stack Size: %u\n", uxTaskGetStackHighWaterMark(NULL));
+  }
+  file.close();
+
+  free(i2s_read_buff);
+  i2s_read_buff = NULL;
+  free(flash_write_buff);
+  flash_write_buff = NULL;
+
+  // listSD_MMC();
+  vTaskDelete(NULL);
+}
+
+void example_disp_buf(uint8_t* buf, int length)
+{
+  printf("======\n");
+  for (int i = 0; i < length; i++) {
+    printf("%02x ", buf[i]);
+    if ((i + 1) % 8 == 0) {
+      printf("\n");
+    }
+  }
+  printf("======\n");
+}
+
+void wavHeader(byte* header, int wavSize) {
+  header[0] = 'R';
+  header[1] = 'I';
+  header[2] = 'F';
+  header[3] = 'F';
+  unsigned int fileSize = wavSize + headerSize - 8;
+  header[4] = (byte)(fileSize & 0xFF);
+  header[5] = (byte)((fileSize >> 8) & 0xFF);
+  header[6] = (byte)((fileSize >> 16) & 0xFF);
+  header[7] = (byte)((fileSize >> 24) & 0xFF);
+  header[8] = 'W';
+  header[9] = 'A';
+  header[10] = 'V';
+  header[11] = 'E';
+  header[12] = 'f';
+  header[13] = 'm';
+  header[14] = 't';
+  header[15] = ' ';
+  header[16] = 0x10;
+  header[17] = 0x00;
+  header[18] = 0x00;
+  header[19] = 0x00;
+  header[20] = 0x01;
+  header[21] = 0x00;
+  header[22] = 0x01;
+  header[23] = 0x00;
+  header[24] = 0x80;
+  header[25] = 0x3E;
+  header[26] = 0x00;
+  header[27] = 0x00;
+  header[28] = 0x00;
+  header[29] = 0x7D;
+  header[30] = 0x00;
+  header[31] = 0x00;
+  header[32] = 0x02;
+  header[33] = 0x00;
+  header[34] = 0x10;
+  header[35] = 0x00;
+  header[36] = 'd';
+  header[37] = 'a';
+  header[38] = 't';
+  header[39] = 'a';
+  header[40] = (byte)(wavSize & 0xFF);
+  header[41] = (byte)((wavSize >> 8) & 0xFF);
+  header[42] = (byte)((wavSize >> 16) & 0xFF);
+  header[43] = (byte)((wavSize >> 24) & 0xFF);
+
+}
+void listFiles(String dirName) {
+  File root = SD_MMC.open(dirName);
+
+  if (!root) {
+    Serial.println("Failed to open directory!");
+    return;
+  }
+
+  if (!root.isDirectory()) {
+    Serial.println("Not a directory!");
+    return;
+  }
+
+  File file = root.openNextFile();
+  while (file) {
+    if (file.isDirectory()) {
+      Serial.print("  DIR : ");
+      Serial.println(file.name());
+      listFiles(file.name());
+    } else {
+      Serial.print("  FILE: ");
+      Serial.print(file.name());
+      Serial.print("  SIZE: ");
+      Serial.println(file.size());
+    }
+    file = root.openNextFile();
+  }
 }
