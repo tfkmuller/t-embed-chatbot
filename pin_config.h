7242e31798b755b69e07d636711d74264a10fb4f
Tom <33751239+tfkmuller@users.noreply.github.com>
2023-04-12 14:14:27 +0100

    Update pin_config.h

--- pin_config.h
+++ pin_config.h
@@ -7,15 +7,8 @@
 
 #define WIFI_CONNECT_WAIT_MAX (30 * 1000)
 
-#define NTP_SERVER1           "pool.ntp.org"
-#define NTP_SERVER2           "time.nist.gov"
-#define GMT_OFFSET_SEC        (3600 * 8)
-#define DAY_LIGHT_OFFSET_SEC  0
+#define WIFI_CONNECT_WAIT_MAX (30 * 1000)
 
-#define LV_SCREEN_WIDTH       320
-#define LV_SCREEN_HEIGHT      170
-#define LV_BUF_SIZE           (LV_SCREEN_WIDTH * LV_SCREEN_HEIGHT)
-/*ESP32S3*/
 #define PIN_POWER_ON          46
 
 #define PIN_IIC_SDA           18
@@ -51,27 +44,13 @@
 #define PIN_SD_MOSI           41
 #define PIN_SD_MISO           38
 
-#define SAMPLES                  512
-//#define SAMPLE_BLOCK             64
-#define SAMPLE_BLOCK             1024
-#define SAMPLE_FREQ              16000
-
-#define I2S_PORT I2S_NUM_0
-#define I2S_SAMPLE_BITS   (16)
-#define I2S_READ_LEN      (16 * 1024)
-#define RECORD_TIME       (20) //Seconds
-#define I2S_CHANNEL_NUM   (1)
-#define RECORD_SIZE (I2S_CHANNEL_NUM * SAMPLE_FREQ * I2S_SAMPLE_BITS / 8 * RECORD_TIME)
-
-#define NUM_LEDS 7
-#define DATA_PIN 42
-#define CLOCK_PIN 45
-
 #define PIN_IN1 2
 #define PIN_IN2 1
 
-#define color1 TFT_WHITE
-#define color2  0x8410
-#define color3 TFT_ORANGE
-#define color4 TFT_BLACK  //background
-#define color5 0xC638
+#define ESP32_RTOS
+
+#define VAD_SAMPLE_RATE_HZ              16000
+#define VAD_FRAME_LENGTH_MS             30
+#define VAD_BUFFER_LENGTH               (VAD_FRAME_LENGTH_MS * VAD_SAMPLE_RATE_HZ / 1000)
+#define I2S_CH                          I2S_NUM_1
+#define SAMPLE_BUFFER_SIZE 512
